df37946a7f28e534e778782dcabea28bc9279ffe
Zach Waterfield <zlwaterfield@gmail.com>
2016-11-26 18:15:52 -0500

    Simon says

--- simon_says/simon_says.js
+++ simon_says/simon_says.js
@@ -1,21 +1,18 @@
 var sequence = []
-
-function initialize () {
-
-}
+var user_clicks = []
 
 function restartSimonSays () {
-  var sequence = []
-  $('.startSimonSays').css({ display: 'block' })
+  sequence = []
+  user_clicks = []
+  $('.startSimonSays').css({ display: 'inline-block' })
   $('.yourTurn').css({ display: 'none' })
+  $('.error').css({ display: 'none' })
+  $('.winner').css({ display: 'none' })
 }
 
 function startSimonSays () {
   $('.startSimonSays').css({ display: 'none' })
-  var next = genRandomNum()
-  console.log(next)
-  sequence.push(next)
-  lightSequence()
+  nextRound()
 }
 
 function genRandomNum () {
@@ -23,18 +20,77 @@ function genRandomNum () {
 }
 
 function lightSequence () {
-  for (var i = 0; i < sequence.length; i++) {
-    var className = mapNumToString(sequence[i])
-    console.log(className)
-    var el =  $('.' +  className)
-    console.log(el)
-    el.val('click')
-    var src = el.currentSrc
-  }
-  $('.yourTurn').css({ display: 'block' })
+  (function myLoop (i) {
+     setTimeout(function () {
+        $('.yourTurn').css({ display: 'none' })
+        var className = mapNumToString(sequence[i])
+        var el =  $('.' +  className)
+        el.find('img').attr('src', getImageUrl(className, 'bright'))
+        dim(className, el)
+
+        if (i < sequence.length - 1) {
+          i++
+          myLoop(i)
+        } else {
+          $('.yourTurn').css({ display: 'inline-block' })
+        }
+     }, 600)
+  })(0);
+}
+
+function dim (className, el, time) {
+  setTimeout(function () {
+    el.find('img').attr('src', getImageUrl(className, 'dim'))
+  }, time || 400)
 }
 
 function simonClick (button) {
+  var el =  $('.' +  button)
+  el.find('img').attr('src', getImageUrl(button, 'bright'))
+  dim(button, el, 100)
+  user_clicks.push(mapStringToNum(button))
+  if (user_clicks.length === sequence.length) {
+    doneRound()
+  } else {
+    checkUserClicks()
+  }
+}
+
+function checkUserClicks () {
+  for (var i = 0; i < user_clicks.length; i++) {
+    if (sequence[i] !== user_clicks[i]) {
+      return userError()
+    }
+  }
+}
+
+function doneRound () {
+  // Check users input
+  for (var i = 0; i < sequence.length; i++) {
+    if (sequence[i] !== user_clicks[i]) {
+      return userError()
+    }
+    if (i === sequence.length - 1) {
+      return nextRound()
+    }
+  }
+}
+
+function userError () {
+  $('.yourTurn').css({ display: 'none' })
+  $('.error').css({ display: 'inline-block' })
+}
+
+function nextRound () {
+  if (sequence.length < 3) {
+    user_clicks = []
+    var next = genRandomNum()
+    sequence.push(next)
+    lightSequence()
+  } else {
+    $('.yourTurn').css({ display: 'none' })
+    $('.winner').css({ display: 'inline-block' })
+  }
 }
 
 function mapStringToNum (string) {
@@ -63,5 +119,15 @@ function mapNumToString (num) {
   }
 }
 
-
-initialize()
+function getImageUrl (className, type) {
+  switch (className) {
+    case 'top_left':
+      return 'img/green_' + type + '.jpg';
+    case 'top_right':
+      return 'img/red_' + type + '.jpg';
+    case 'bottom_left':
+      return 'img/yellow_' + type + '.jpg';
+    case 'bottom_right':
+      return 'img/blue_' + type + '.jpg';
+  }
+}
