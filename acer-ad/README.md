cd836280e467caf8d8f5beaaaa01ac1e3f120f02
Acer Wang <acer.chi.wang@gmail.com>
2016-11-26 14:27:18 -0500

    [GET] /api/ad feature added

--- acer-ad/README.md
+++ acer-ad/README.md
@@ -1,6 +1,11 @@
 #acer serves ad
 not really. Python Flask that serves fake ads
 
+## Dependancies
+- python 
+- pip
+- virtualenv
+
 ## GET - /api/ad
 no arguments.
 
--- acer-ad/app.py
+++ acer-ad/app.py
@@ -1,8 +1,32 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify, abort
+import random
 
 app = Flask(__name__)
 
+pic_base_url = "http://localhost.5000/pics"
+shitty_url = [
+    "penisland",
+    "speedofart",
+    "masterbaitonline",
+    "gotahoe",
+    "mofo",
+    "bendover",
+    "pedo",
+    "nycanal",
+    "auctionshit",
+    "teacherstalk"
+]
+
 @app.route('/')
 def index():
-    return "Hellow World!"
+    return "Hello World!"
+
+@app.route('/api/ad', methods=['GET'])
+def server_ads():
+    final_filename = pic_base_url + '/0' + str(random.randint(1,9)) + '.jpg'
+    final_url = 'http://www.' + shitty_url[random.randint(0,9)] + '.com' 
+    return jsonify({'picture': final_filename, 'url': final_url})
+
+if __name__ == '__main__':
+    app.run(debug=True)
